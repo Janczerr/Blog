28c76faa9324d06032dc4b944b0db26df475d74f
Janczerr <lukasz.jankowwski@gmail.com>
2021-05-25 22:15:40 +0200

    Changed width to clamp and added comments

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 function addArticle(){
-
+    //Adding div container
     let article = document.createElement('div');
     article.classList.add('blog-card');
-
     document.body.appendChild(article);
 
-    //@ts-ignore
+    //@ts-ignore, Adding the header
     let articleTitle = document.getElementById('articleTitle').value;
     let header = document.createElement('h4');
     header.innerHTML = articleTitle;
-
     article.append(header);
 
-    //@ts-ignore
+    //@ts-ignore, Adding the description
     let articleDescription = document.getElementById('articleDescription').value;
     let description = document.createElement('p');
     description.innerHTML = articleDescription;
-
     article.append(description);
 
+    //Adding the remove button
     let remove = document.createElement('a');
     remove.classList.add('btn-delete');
     remove.innerHTML = "Remove";
